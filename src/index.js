c128289650ae5e16f8a3605c626e76dba8e432ab
Adithyan Ashok <adithyanashok406@gmail.com>
2022-10-03 12:26:00 +0530

    axios error

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 import { persistor, store } from './redux/store';
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
+import axios from 'axios'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+axios.defaults.withCredentials = true
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
