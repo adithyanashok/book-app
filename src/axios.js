780c899abac205e1fbea5d1e5ba14f5c493e2d67
Adithyan Ashok <adithyanashok406@gmail.com>
2022-12-07 15:33:22 +0530

    cors

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL: 'https://book-review-backend-production.up.railway.app/api/',
-    withCredentials: true
+  baseURL: "https://book-review-backend-production.up.railway.app/api/",
+  withCredentials: true,
 });
